7c3299b04963329bc7931c125210ef5d7ac7d497
prabhak-rvs <prabhak@rvsgroup.com>
2026-09-22 13:36:06 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,10 +1,1 @@
-SET SERVEROUTPUT ON
 
-DECLARE
-    num1 NUMBER := 10;
-    num2 NUMBER := 20;
-    total NUMBER;
-BEGIN
-    total := num1 + num2;
-    DBMS_OUTPUT.PUT_LINE('Sum = ' || total);
-END;
