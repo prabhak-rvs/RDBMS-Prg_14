2a2680b4c0985a17a4afbb18c89e99b80c7faa7f
prabhak-rvs <prabhak@rvsgroup.com>
2026-09-22 13:33:32 +0530

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,10 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    num1 NUMBER := 10;
+    num2 NUMBER := 20;
+    total NUMBER;
+BEGIN
+    total := num1 + num2;
+    DBMS_OUTPUT.PUT_LINE('Sum = ' || total);
+END;
